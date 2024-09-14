a2d51224c47eea379f8c6df0373010f8f5f1bba9
rehanshaikh25 <shakilrehan23@gmail.com>
2024-09-15 01:18:19 +0530

    final commit for 2nite

--- main.js
+++ main.js
@@ -122,16 +122,10 @@ function render(todo) {
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                     </svg>
                 </button>
-                <button class="text-blue-400 hover:text-blue-300">
-                    <!-- Pencil Icon -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536-9.897 9.898-3.536-3.536L15.232 5.232z" />
-                    </svg>
-                </button>
-                <button class="deleteBtn text-red-400 hover:text-red-300">
+                <button class="deleteBtn text-gray-400 hover:text-red-400">
                     <!-- Trash Icon -->
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                     </svg>
                 </button>
             </div>
@@ -156,6 +150,9 @@ function render(todo) {
 
         // Update the SVG based on the completion status
         const svg = todoItem.querySelector('svg');
+        
+        svg.classList.toggle('text-green-400')
+
         if (todo.isComplete) {
             // Replace SVG with the completed state
             svg.innerHTML = `
